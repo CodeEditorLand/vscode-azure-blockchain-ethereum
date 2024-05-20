e43292aa729c7a4b17ca92d76a45d6dfa4ed67ed
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-20 15:26:40 +0300

    squash!

--- src/ARMBlockchain/AzureDto/AccessKeysDto.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-export interface IAzureMemberAccessKeysDto {
-  keys: [{
-    keyName: string;
-    value: string
-  }];
-}
--- src/ARMBlockchain/AzureDto/CreateTransactionNodeDto.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-export interface ICreateTransactionNodeDto {
-  location: string;
-  properties: {
-    password: string;
-  };
-}
