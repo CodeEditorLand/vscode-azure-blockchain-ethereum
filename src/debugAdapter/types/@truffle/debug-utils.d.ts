921cc807cbb1253d8f3503e5a978bc4686fe186b
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-06 01:31:51 +0300

    squash!

--- src/debugAdapter/types/@truffle/debug-utils.d.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-declare module '@truffle/debug-utils' {
-  function nativize(variables: any): any;
-
-  export { nativize };
-}
--- src/services/extensionAdapter/IExtensionAdapter.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-export interface IExtensionAdapter {
-  validateExtension: () => Promise<void>;
-  build: () => Promise<void>;
-  deploy: () => Promise<void>;
-}
